fb2724ce57ba96bcf567a6f51d9f7dd74e06ac7e
karben86 <benjaminkarst86@gmail.com>
2020-10-13 17:10:55 +0100

    added more comments to get-tweet-data problem

--- get-tweet-data.js
+++ get-tweet-data.js
@@ -1,7 +1,7 @@
 // Please do not change the name of this function
 function getTweetData(tweet) {
 
-  // The whole tweet length is taken into account.
+  // The whole tweet length is taken into account and for instances where there is a url we add all the url lengths again as twitter only takes 50% into account.
   const length = tweet.length + (tweet.match(/http:\/\/.*?(?= |$)/g) || [""]).map(string => string.length).reduce((total, urlLength) => total + urlLength);
 
   // Mentions are defined by any word that starts with @. These are all counted and subsequently printed by the below regex,
@@ -14,9 +14,10 @@ function getTweetData(tweet) {
   const tagCount = (tweet.match(/(?<= |^)#\b(\w*)\b(?!.*\1)/g) || "").length;
   const tags = tweet.match(/(?<= |^)#\b(\w*)\b(?!.*\1)/g) || [];
 
-  //url is defined as any string that starts with http:// and is counted by following a similar logic to tags and mentions.
+  // url is defined as any string that starts with http:// and is counted by following a similar logic to tags and mentions.
   const urlCount = (tweet.match(/http:\/\/\w+/g) || "").length;
 
+  // email is defined as any word that contains the character @ between at least 2 letters.
   const email = (tweet.match(/\w@\w/g) || "").length;
 
   // Return the required object litteral with the entries populated according to the above.
