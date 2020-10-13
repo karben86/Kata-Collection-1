c9d5e116da1999e79f06ced3fc4b397bc5fcc6e4
karben86 <benjaminkarst86@gmail.com>
2020-10-13 10:43:13 +0100

    almost finished get-tweet-data but still working 50% url problem

--- get-tweet-data.js
+++ get-tweet-data.js
@@ -2,21 +2,26 @@
 function getTweetData(tweet) {
 
   // The whole tweet length is taken into account.
-  const length = tweet.length
+  const length = tweet.length + (tweet.match(/http:\/\/\w+(?= |$)/g) || [""]).map(string => string.length).reduce((total, urlLength) => total + urlLength);
 
   // Mentions are defined by any word that starts with @. These are all counted and subsequently printed by the below regex,
   // except for cases where the same exact mention occurs multiple times in which case only one instance is taken into account.
   // Since match will result in null when no match occurs, we have to instead print out an empty string or an empty array for this exercise.
-  const mentionCount = (tweet.match(/@\b(\w*)\b(?!.*\1)/g) || "").length;
-  const mentions = tweet.match(/@\b(\w*)\b(?!.*\1)/g) || [];
+  const mentionCount = (tweet.match(/(?<= |^)@\b(\w*)\b(?!.*\1)/g) || "").length;
+  const mentions = tweet.match(/(?<= |^)@\b(\w*)\b(?!.*\1)/g) || [];
 
-  // The logic for tweets is exactly the same as that for mentions so the regex is identical except for the # instead of the @.
-  const tagCount = (tweet.match(/#\b(\w*)\b(?!.*\1)/g) || "").length;
-  const tags = tweet.match(/#\b(\w*)\b(?!.*\1)/g) || [];
+  // The logic for tags is exactly the same as that for mentions so the regex is identical except for the # instead of the @.
+  const tagCount = (tweet.match(/(?<= |^)#\b(\w*)\b(?!.*\1)/g) || "").length;
+  const tags = tweet.match(/(?<= |^)#\b(\w*)\b(?!.*\1)/g) || [];
+
+  //url is defined as any string that starts with http:// and is counted by following a similar logic to tags and mentions.
+  const urlCount = (tweet.match(/http:\/\/\w+/g) || "").length;
+
+  const email = (tweet.match(/\w@\w/g) || "").length;
 
   // Return the required object litteral with the entries populated according to the above.
   // Since all the variables were named according to the desired output we do not need to list key-value pairs separately.
-  return { tags, mentions, tagCount, mentionCount, length };
+  return { tags, mentions, tagCount, mentionCount, length, urlCount, email };
 };
 
 module.exports = getTweetData;
--- test/get-tweet-data.test.js
+++ test/get-tweet-data.test.js
@@ -1,21 +1,29 @@
 const getTweetData = require('../get-tweet-data.js');
 
-test('getTweetData("My awesome tweet") should return "{ tags: [], mentions: [], tagCount: 0, mentionCount: 0, length: 16 }"', () => {
-    expect(getTweetData("My awesome tweet")).toEqual({ tags: [], mentions: [], tagCount: 0, mentionCount: 0, length: 16 });
+test('getTweetData("My awesome tweet") should return "{ tags: [], mentions: [], tagCount: 0, mentionCount: 0, length: 16, urlCount: 0}"', () => {
+    expect(getTweetData("My awesome tweet")).toEqual({ tags: [], mentions: [], tagCount: 0, mentionCount: 0, length: 16, urlCount: 0, email: 0 });
 });
 
-test('getTweetData("My awesome tweet to @northcoders") should return "{ tags: [], mentions: [\'@northcoders\'], tagCount: 0, mentionCount: 1, length: 32 }"', () => {
-    expect(getTweetData("My awesome tweet to @northcoders")).toEqual({ tags: [], mentions: ['@northcoders'], tagCount: 0, mentionCount: 1, length: 32 });
+test('getTweetData("My awesome tweet to @northcoders") should return "{ tags: [], mentions: [\'@northcoders\'], tagCount: 0, mentionCount: 1, length: 32, urlCount: 0}"', () => {
+    expect(getTweetData("My awesome tweet to @northcoders")).toEqual({ tags: [], mentions: ['@northcoders'], tagCount: 0, mentionCount: 1, length: 32, urlCount: 0, email: 0 });
 });
 
-test('getTweetData("My awesome tweet about #coding") should return "{ tags: [\'#coding\'], mentions: [], tagCount: 1, mentionCount: 0, length: 30 }"', () => {
-    expect(getTweetData("My awesome tweet about #coding")).toEqual({ tags: ['#coding'], mentions: [], tagCount: 1, mentionCount: 0, length: 30 });
+test('getTweetData("My awesome tweet about #coding") should return "{ tags: [\'#coding\'], mentions: [], tagCount: 1, mentionCount: 0, length: 30, urlCount: 0  }"', () => {
+    expect(getTweetData("My awesome tweet about #coding")).toEqual({ tags: ['#coding'], mentions: [], tagCount: 1, mentionCount: 0, length: 30, urlCount: 0, email: 0 });
 });
 
-test('getTweetData("My awesome tweet about #coding to @northcoders") should return "{ tags: [\'#coding\'], mentions: [\'@northcoders\'], tagCount: 1, mentionCount: 1, length: 46 }"', () => {
-    expect(getTweetData("My awesome tweet about #coding to @northcoders")).toEqual({ tags: ['#coding'], mentions: ['@northcoders'], tagCount: 1, mentionCount: 1, length: 46 });
+test('getTweetData("My awesome tweet about #coding to @northcoders") should return "{ tags: [\'#coding\'], mentions: [\'@northcoders\'], tagCount: 1, mentionCount: 1, length: 46, urlCount: 0  }"', () => {
+    expect(getTweetData("My awesome tweet about #coding to @northcoders")).toEqual({ tags: ['#coding'], mentions: ['@northcoders'], tagCount: 1, mentionCount: 1, length: 46, urlCount: 0, email: 0 });
 });
 
-test('getTweetData("I am #coding with @northcoders I love #coding and @northcoders") should return { tags: [\'#coding\'], mentions: [\'@northcoders\'], tagCount: 1, mentionCount: 1, length: 62 }"', () => {
-    expect(getTweetData("I am #coding with @northcoders I love #coding and @northcoders")).toEqual({ tags: ['#coding'], mentions: ['@northcoders'], tagCount: 1, mentionCount: 1, length: 62 });
+test('getTweetData("I am #coding with @northcoders I love #coding and @northcoders") should return { tags: [\'#coding\'], mentions: [\'@northcoders\'], tagCount: 1, mentionCount: 1, length: 62, urlCount: 0  }"', () => {
+    expect(getTweetData("I am #coding with @northcoders I love #coding and @northcoders")).toEqual({ tags: ['#coding'], mentions: ['@northcoders'], tagCount: 1, mentionCount: 1, length: 62, urlCount: 0, email: 0 });
+});
+
+test('getTweetData("My awesome tweet about #coding to @northcoders includes some url called http://www.someurl.com") should return "{ tags: [\'#coding\'], mentions: [\'@northcoders\'], tagCount: 1, mentionCount: 1, length: 94, urlCount: 1  }"', () => {
+    expect(getTweetData("My awesome tweet about #coding to @northcoders includes some url called http://www.someurl.com")).toEqual({ tags: ['#coding'], mentions: ['@northcoders'], tagCount: 1, mentionCount: 1, length: 94, urlCount: 1, email: 0 });
+});
+
+test('getTweetData("I am #coding with @northcoders and my email address is example@fake.com") should return { tags: [\'#coding\'], mentions: [\'@northcoders\'], tagCount: 1, mentionCount: 1, length: 71, urlCount: 0, email: 0  }"', () => {
+    expect(getTweetData("I am #coding with @northcoders and my email address is example@fake.com")).toEqual({ tags: ['#coding'], mentions: ['@northcoders'], tagCount: 1, mentionCount: 1, length: 71, urlCount: 0, email: 1 });
 });
