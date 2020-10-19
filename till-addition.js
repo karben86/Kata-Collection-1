cd47a0e888990e62a9c049bca4a087550d92b63f
karben86 <benjaminkarst86@gmail.com>
2020-10-19 11:17:02 +0100

    adding comments to tillAddition.

--- till-addition.js
+++ till-addition.js
@@ -6,7 +6,7 @@ function tillAddition(cash) {
 
     //and then multiplying each coin/note (element[0]) with the quantity we hold (element[1]).
     //in order to use the coins and notes in our calculation we need to convert them first into the correct monetary value.
-    //anything preceded by a pound sign is converted to a pure number i.e. £10 is converted to 10.
+    //Anything preceded by a pound sign is converted to a pure number i.e. £10 is converted to 10.
     //Anything succeeded by a p is converted to a pure number and divided by 100 i.e. 10p is converted to 0.01.
     .map(element => element[0].replace("£", "").replace(/(.*\d)p/, (match, group) => group / 100) * element[1])
 
